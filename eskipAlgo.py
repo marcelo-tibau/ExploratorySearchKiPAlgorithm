f6303fcc979c069a13064a2c4f3350cc779e1c14
marcelo_tibau <marcelo@tibau.org>
2018-09-15 16:52:40 -0300

    algorithm structure

--- eskipAlgo.py
+++ eskipAlgo.py
@@ -1,1 +1,11 @@
 #Exploratory Search KiP Algorithm
+  
+class NoChoicesLeft(Error): pass
+
+def choose(*choices):
+    for choice in choices:
+        try:
+            return choice()
+        except NoChoicesLeft:
+            continue
+
