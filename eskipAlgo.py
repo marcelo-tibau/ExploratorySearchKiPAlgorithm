9600436436ab57768a3c8d392120a1ffe4baeca2
marcelo_tibau <marcelo@tibau.org>
2018-09-29 17:39:57 -0300

    update two ongoing

--- eskipAlgo.py
+++ eskipAlgo.py
@@ -9,3 +9,38 @@ def choose(*choices):
         except NoChoicesLeft:
             continue
 
+        
+class KeyWord:
+    
+    def __init__(self, filenames):
+        self.filenames = filenames
+        self.index = buildindex.BuildIndex(self.filenames)
+        self.invertedIndex = self.index.totalIndex
+        self.regularIndex = self.index.regdex
+
+
+    def oneWordKeyWord(self, word):
+        pattern = re.compile('[\W_]+')
+        word = pattern.sub(' ', word)
+        if word in self.invertedIndex.keys():
+            return self.rankResults([filename for filename in self.invertedIndex[word].keys()], word)
+        else:
+            return []
+
+	
+        
+        
+##########################################################
+#choose X satisfying P(X) {
+#	init var list Q = []
+			
+#		compare q1 to q1+1 { 
+#			if q1 < q1+1 & q1+1 contains q1
+#				q1 == K1 & q1+1 == K2
+#			else if q1 !== q1+1
+#				q1+1 == K3
+#			else
+#				q1 == K4
+#			add to list Q
+#			}
+#		end compare
